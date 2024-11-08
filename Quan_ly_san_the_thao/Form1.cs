c578510cc6a7930ded77b5ac252c33a1739b52a3
Khanh Duy <94819005+PhDuy2005@users.noreply.github.com>
2024-11-08 15:52:08 +0700

    Update README.txt

--- Quan_ly_san_the_thao/Form1.cs
+++ Quan_ly_san_the_thao/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Quan_ly_san_the_thao
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
--- Quan_ly_san_the_thao/README.txt
+++ Quan_ly_san_the_thao/README.txt
@@ -1,1 +1,5 @@
-Nothing yet to be read!
+﻿Quy ước cách đặt tên:
+Button: btn_[tên]
+Radio Button: rdBtn_[tên]
+ListView: lv_[tên]
+ComboBox: cBox_[tên]
