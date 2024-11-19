4add37a30b954a54d004efa6fea4ee985898fd2a
Khanh Duy <94819005+PhDuy2005@users.noreply.github.com>
2024-11-20 00:45:13 +0700

    Hoàn thiện form chọn thời gian

--- Quan_ly_san_the_thao/SportListForm.Designer.cs
+++ Quan_ly_san_the_thao/SportListForm.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1034, 863);
+            this.ClientSize = new System.Drawing.Size(797, 477);
             this.Name = "SportListForm";
             this.Text = "SportListForm";
             this.ResumeLayout(false);
